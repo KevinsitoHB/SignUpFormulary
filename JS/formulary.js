4c1789c8083c518eb632597b97ecdd56e07535c1
KevinsitoHB <family-hernandez2014@outlook.com>
2024-04-24 14:54:48 -0400

    mas funciones

--- JS/formulary.js
+++ JS/formulary.js
@@ -5,25 +5,31 @@ const d = document;
 const $signUpForm = d.getElementById('signUpForm');
 
 d.addEventListener('DOMContentLoaded', () => {
-  eventListenerS();
+  eventListeners();
 });
 
-function eventListenerS() {
+//VARIABLES
+const credentials = {
+  name: $signUpForm.name.value.trim(),
+  email: $signUpForm.email.value.trim(),
+  phone: $signUpForm.phone.value.trim(),
+  password: $signUpForm.password2.value.trim(),
+  password2: $signUpForm.password.value.trim(),
+};
+
+// EVENTS
+function eventListeners() {
   $signUpForm.addEventListener('submit', handleSubmit);
+  $signUpForm.name.addEventListener('blur', checkNameBlur);
 }
+
+//FUNCTIONS
 
 function handleSubmit(event) {
   event.preventDefault();
 
-  const credentials = {
-    id: '000001',
-    name: $signUpForm.name.value,
-    email: $signUpForm.email.value,
-    password: $signUpForm.password.value,
-  };
-
-  const returnedValues = validateCredentials(credentials);
-  if (returnedValues) {
+  const validCredentials = areValidCredentials(credentials);
+  if (validCredentials) {
     console.log('Send data');
   } else {
     console.log("Don't send data");
@@ -33,6 +39,33 @@ function handleSubmit(event) {
   // const formData = new FormData($signUpForm);
   // console.log(formData);
 }
-function validateCredentials(userCredentials) {
+function checkNameBlur() {
+  credentials.name = $signUpForm.name.value.trim();
+  console.log(credentials.name);
+  const $warningDivName = d.getElementById('warningDivName');
+  if (credentials.name == '') {
+    $warningDivName.textContent = 'Please enter your name';
+  } else {
+    $warningDivName.textContent = '';
+  }
+}
+
+function areValidCredentials(userCredentials) {
   console.log(userCredentials);
+  const name = userCredentials.name.trim();
+  const email = userCredentials.email.trim();
+  const phone = userCredentials.phone.trim();
+  const password = userCredentials.password.trim();
+  const password2 = userCredentials.password2.trim();
+  if (
+    name === '' ||
+    email === '' ||
+    phone === '' ||
+    password === '' ||
+    password2 === ''
+  ) {
+    return false;
+
+    return true;
+  }
 }
